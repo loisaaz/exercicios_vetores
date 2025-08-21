0e4ab461bc18ed63871c27ae33c72e1aed3ff0b9
heloisa <125582181+loisaaz@users.noreply.github.com>
2025-08-21 16:15:57 -0300

    feito exercicio 1
    Faça um programa que faz a leitura de dois vetores X e Y, ambos com N reais (N deve ser lido também), com tamanho máximo igual a 20. Efetue a multiplicação dos respectivos pares de elementos de X por Y, gerando um terceiro vetor Z. Imprima os vetores X, Y e Z.

---
+++ ex1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int x[21], y[21], z[21];
+    int N, cont;
+    
+    printf("\n\tDigite quantos números serão lidos (máx 20): ");
+    scanf("%d", &N);
+    
+    if (N > 20)
+    {
+        printf("\n\tO tamanho máximo é 20, encerrando programa...");
+        return 1;
+        
+    }
+
+    for (cont = 0;  cont < N; cont ++)
+    {
+        //printf("\n\tVetor X");
+        printf("\n\tDigite o número [%d]: ", cont);
+        scanf("%d", &x[cont]);
+    }
+
+    for (cont = 0;  cont < N; cont ++)
+    {
+        //printf("\n\tVetor Y");
+        printf("\n\tDigite o número [%d]:", cont);
+        scanf("%d", &y[cont]);
+    }
+    
+    for (cont = 0; cont < N; cont++)
+    {
+        z[cont] = x[cont] * y[cont];
+    }
+    
+    printf("\n\tVetor X: ");
+    for (cont = 0; cont < N; cont++) {
+        printf("%d ", x[cont]);
+    }
+    
+    printf("\n\tVetor Y: ");
+    for (cont = 0; cont < N; cont++) {
+        printf("%d ", y[cont]);
+    }
+    
+    printf("\n\tVetor Z: ");
+    for (cont = 0; cont < N; cont++) {
+        printf("%d ", z[cont]);
+    }
+
+    return 0;
+}
